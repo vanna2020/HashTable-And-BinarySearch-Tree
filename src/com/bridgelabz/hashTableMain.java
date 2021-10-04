08c3dc79a6b05649bd41614168489df4c28d1a47
vanna2020 <vannasingh1999@gmail.com>
2021-10-03 20:52:45 -0700

    Ability To Find Frequqncy of words in large Paragraph

--- src/com/bridgelabz/hashTableMain.java
+++ src/com/bridgelabz/hashTableMain.java
@@ -1,24 +1,24 @@
 package com.bridgelabz;
-    public class hashTableMain {
-        public static void main(String[] args) {
-            MyHashTable<String, Integer> hashTable = new MyHashTable();
-            String sentence = "To be or not to be";
+class HashTableMain {
+    public static void main(String[] args) {
+        MyHashMap<String, Integer> hashTable = new MyHashMap();
+        String sentence = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
 
         /* Storing given sentence in an array.
            Converting the sentence to lower case and removing spaces.
         */
-            String[] sentenceArray = sentence.toLowerCase().split(" ");
+        String[] sentenceArray = sentence.toLowerCase().split(" ");
 
-            // Iterating over the array.
-            for (String word : sentenceArray) {
-                Integer count = hashTable.get(word);
+        // Iterating over the array.
+        for (String word : sentenceArray) {
+            Integer count = hashTable.get(word);
 
-                if (count == null)
-                    count = 1;
-                else
-                    count = count + 1;
-                hashTable.add(word, count);
-            }
-            System.out.println(hashTable);
+            if (count == null)
+                count = 1;
+            else
+                count = count + 1;
+            hashTable.add(word, count);
         }
+        System.out.println(hashTable);
     }
+}
