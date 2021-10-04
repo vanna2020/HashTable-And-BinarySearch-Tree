c65ce3c2385d4aec1dcaa240e4da5569492238c4
vanna2020 <vannasingh1999@gmail.com>
2021-10-03 21:17:00 -0700

    Ability to find frequency of words in large paragraph

--- src/com/bridgelabz/hashTableMain.java
+++ src/com/bridgelabz/hashTableMain.java
@@ -1,25 +1,27 @@
 package com.bridgelabz;
+
 class HashTableMain {
-    public static void main(String[] args) {
-        MyHashMap<String, Integer> hashTable = new MyHashMap();
-        String sentence = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+
+        public static void main(String[] args) {
+            MyHashMap<String, Integer> hashTable = new MyHashMap();
+            String sentence = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
 
         /* Storing given sentence in an array.
            Converting the sentence to lower case and removing spaces.
         */
-        String[] sentenceArray = sentence.toLowerCase().split(" ");
+            String[] sentenceArray = sentence.toLowerCase().split(" ");
 
-        // Iterating over the array.
-        for (String word : sentenceArray) {
-            Integer count = hashTable.get(word);
+            // Iterating over the array.
+            for (String word : sentenceArray) {
+                Integer count = hashTable.get(word);
 
-            if (count == null)
-                count = 1;
-            else
-                count = count + 1;
-            hashTable.add(word, count);
+                if (count == null)
+                    count = 1;
+                else
+                    count = count + 1;
+                hashTable.add(word, count);
+            }
+            hashTable.remove("avoidable");
+            System.out.println(hashTable);
         }
-        hashTable.remove("avoidable");
-        System.out.println(hashTable);
     }
-}
